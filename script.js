0649c4a7ebc388ef8ac6b1fd7543bc936d336c65
Priyadharshini-09 <160256832+Priyadharshini-09@users.noreply.github.com>
2024-12-28 23:59:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const bars = document.querySelector('.fa-bars');
+const navlist = document.querySelector('.navlist');
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
